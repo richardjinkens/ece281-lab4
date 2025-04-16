e8c64f8398d6b36c73799a6d44f39a0c50e3a003
RichardJinkens <177856237+richardjinkens@user.noreply.github.com>
2025-04-15 19:45:08 -0600

    second elevator

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -27,6 +27,7 @@ architecture top_basys3_arch of top_basys3 is
     -- signal declarations
     signal w_fsm_reset    : std_logic;
     signal w_fsm_out1     : std_logic_vector (3 downto 0);
+    signal w_fsm_out2     : std_logic_vector (3 downto 0);
     signal w_slowclk      : std_logic;
     
     signal w_tdm4_reset   : std_logic;
@@ -34,7 +35,11 @@ architecture top_basys3_arch of top_basys3 is
     
     signal w_data1        : std_logic_vector (3 downto 0); --F
     signal w_data3        : std_logic_vector (3 downto 0); --F
-    signal w_data0        : std_logic_vector (3 downto 0); --second elevator placeholder
+    
+    -- testing
+    signal w_an : std_logic_vector(3 downto 0);
+
+    
     
   
 	-- component declarations
@@ -90,41 +95,62 @@ begin
         sevenseg_unit : sevenseg_decoder
             port map (
                 i_Hex       => w_tdm4_out,
-                o_seg_n     => seg
+                o_seg_n(0) => seg(0),
+                o_seg_n(1) => seg(1),
+                o_seg_n(2) => seg(2),
+                o_seg_n(3) => seg(3),
+                o_seg_n(4) => seg(4),
+                o_seg_n(5) => seg(5),
+                o_seg_n(6) => seg(6)
             );
         
         elevator_unit : elevator_controller_fsm
             port map (
                 i_clk       => w_slowclk,        
                 i_reset     => w_fsm_reset,
+                is_stopped  => sw(0),
+                go_up_down  => sw(1), 
+                o_floor 	=> w_fsm_out1
+            );
+            
+        elevator2_unit : elevator_controller_fsm
+            port map (
+                i_clk       => w_slowclk,        
+                i_reset     => w_fsm_reset,
                 is_stopped  => sw(15),
                 go_up_down  => sw(14), 
-                o_floor 	=> w_fsm_out1
+                o_floor 	=> w_fsm_out2
             );
         
         tdm4_unit : TDM4
             port map (
-                i_clk       => clk,
+                i_clk       => w_slowclk,
                 i_reset		=> w_tdm4_reset,
                 i_D3 		=> w_data3,
 		        i_D2 		=> w_fsm_out1,
 		        i_D1 		=> w_data1,
-		        i_D0 		=> w_data0,
+		        i_D0 		=> w_fsm_out2,
 		        o_data		=> w_tdm4_out,
-		        o_sel		=> an
+		        o_sel		=> w_an
 	        );
+	        
+	    
             
            
 	
 	-- CONCURRENT STATEMENTS ----------------------------
 	-- placeholders for elevators / F display
-	w_data0      <= "0000";
+	--w_data0      <= "0000";
 	w_data1      <= x"F";
 	w_data3      <= x"F";
 	-- LED 15 gets the FSM slow clock signal. The rest are grounded.
 	led(15) <= w_slowclk;
 	-- leave unused switches UNCONNECTED. Ignore any warnings this causes.
+	led(3 downto 0) <= w_tdm4_out;
 	
+	an <= w_an;           -- drive the 7-seg anode lines
+    led(9 downto 6) <= not w_an;  -- invert for visual clarity (0 = ON)
+
 	-- reset signals
 	w_fsm_reset  <= btnR or btnU;
 	w_tdm4_reset <= btnL or btnU;
